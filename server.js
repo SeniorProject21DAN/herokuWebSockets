3c6f5af0c4516e0807c6fe9dc09c1302aa312643
nstrain <30319889+nstrain@users.noreply.github.com>
2022-04-13 22:04:19 -0400

    change variable name and cross fingers

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
+const app = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
+const wss = new Server({ app });
 
 
 // Single dimension array storing name of host, used for host setup and client setup 
